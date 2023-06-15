b858caed7d2bb5f97b7ed8658ac5ba7578a99c18
Kousco <joekousoulos@gmail.com>
2023-06-15 15:47:29 -0700

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,110 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'We have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    subtitle:
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the Metaverse',
+    subtitle:
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
+
+export const styles = {
+  innerWidth: '2xl:max-w-[1280px] w-full',
+  interWidth: 'lg:w-[80%] w-[100%]',
+
+  paddings: 'sm:p-16 xs:p-8 px-6 py-12',
+  yPaddings: 'sm:py-16 xs:py-8 py-12',
+  xPaddings: 'sm:px-16 px-6',
+  topPaddings: 'sm:pt-16 xs:pt-8 pt-12',
+  bottomPaddings: 'sm:pb-16 xs:pb-8 pb-12',
+
+  flexCenter: 'flex justify-center items-center',
+  flexStart: 'flex justify-start items-start',
+  flexEnd: 'flex justify-end',
+  navPadding: 'pt-[98px]',
+
+  // hero section
+  heroHeading:
+      'font-bold lg:text-[144px] md:text-[100px] sm:text-[60px] text-[44px] lg:leading-[158.4px] md:leading-[114.4px] sm:leading-[74.4px] leading-[64.4px] uppercase text-white',
+  heroDText:
+      'md:w-[212px] sm:w-[80px] w-[60px] md:h-[108px] sm:h-[48px] h-[38px] md:border-[18px] border-[9px] rounded-r-[50px] border-white sm:mx-2 mx-[6px]',
+};
---
+++ src/sections/World.jsx
@@ -0,0 +1,61 @@
+'use client';
+
+import { motion } from 'framer-motion';
+
+import { styles } from '../constants/index';
+import {  TitleText, TypingText } from '../components';
+import { staggerContainer, fadeIn } from '../utils/motion';
+
+const World = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText
+        title="| People on the World"
+        textStyles="text-center"
+      />
+      <TitleText
+        title={<>Track friends around you and invite them to play together in the same world</>}
+        textStyles="text-center"
+      />
+      <motion.div
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        className='relative mt-[68px] flex w-full h-[550px] '
+      >
+        <img
+          src="/map.png"
+          alt='map'
+          className='lg:w-full lg:h-full md:h-[70%] h-[50%] lg:object-cover'
+        />
+        <div className='absolute lg:bottom-20 lg:right-[30%] top-10 right-[15%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680] '>
+          <img
+            src='people-01.png'
+            alt='people'
+            className='w-full h-full'
+          />
+        </div>
+        <div className='absolute top-[10%] left-[10%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680] '>
+          <img
+            src='people-02.png'
+            alt='people'
+            className='w-full h-full'
+          />
+        </div>
+        <div className='absolute lg:top-1/2 lg:left-[45%] top-[20%] left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680] '>
+          <img
+            src='people-03.png'
+            alt='people'
+            className='w-full h-full'
+          />
+        </div>
+      </motion.div>
+    </motion.div>
+  </section>
+);
+
+export default World;
